12e0173ea5a1a69dd340f41f74de3c87c74b3193
Cheng Vue <cvu3@ucsc.edu>
2018-11-26 18:47:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ class Song
   def self.new_from_filename(file_name)
     song = self.new
     artist_name = file_name.match(/^\w+\s\w+/)
-    song_name = file_name.scan(/-.+[^\.mp3].join("- ")
+    song_name = file_name.scan(/-.+[^\.mp3]/).join("- ")
     song.name=(song_name)
     song.artist=(artist_name)
   end
